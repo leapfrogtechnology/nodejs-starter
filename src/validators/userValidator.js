6a46a5d43823a9545005792adf5dae62d610d7b6
Robus Gauli <robusgauli@gmail.com>
2020-04-10 17:26:20 +0545

    Remove unused joi validation service

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,7 +1,6 @@
 import Joi from '@hapi/joi';
 
 import validate from '../utils/validate';
-import * as userService from '../services/userService';
 
 // Validation schema
 const schema = Joi.object({
@@ -19,22 +18,7 @@ const schema = Joi.object({
 function userValidator(req, res, next) {
   return validate(req.body, schema)
     .then(() => next())
-    .catch((err) => next(err));
+    .catch(next);
 }
 
-/**
- * Validate users existence.
- *
- * @param   {Object}   req
- * @param   {Object}   res
- * @param   {Function} next
- * @returns {Promise}
- */
-function findUser(req, res, next) {
-  return userService
-    .getUser(req.params.id)
-    .then(() => next())
-    .catch((err) => next(err));
-}
-
-export { findUser, userValidator };
+export { userValidator };
